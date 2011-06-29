db0e3b53b35670f0228938c888deef4051c084ae
Scott Kroll <skroll@gmail.com>
2011-06-29 14:01:16 -0400

    Add comment to custom function autoload.

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,6 +1,7 @@
 ## Load functions that should be publicly available to the user.
 for func ($ZSH/functions/*~*.zwc) autoload -U $func:t
 
+## Load custom functinos that should be publicly available to the user.
 if [ -d $ZSH/custom/functions ];
 then
 	setopt nullglob
