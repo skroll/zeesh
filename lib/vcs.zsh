58e7f8569d3bde10bdd21fd3e126e5ec540234bc
Scott Kroll <skroll@gmail.com>
2011-06-30 15:56:49 -0400

    Conditional vcs prompt support.

--- lib/vcs.zsh
+++ lib/vcs.zsh
@@ -1,6 +1,15 @@
 ## Unified version control system prompt information
 function vcs_prompt_info() {
 	## Try git first, then mercurial
-	ref=$(git_prompt_info) || ref=$(hg_prompt_info) || ref=""
-	echo "${ref}"
+	vcsprompt=""
+
+	[[ "$DISABLE_ZEESH_GIT" != "true" ]] \
+		&& [[ -z "$vcsprompt" ]] \
+		&& vcsprompt=$(git_prompt_info)
+
+	[[ "$DISABLE_ZEESH_HG" != "true" ]] \
+		&& [[ -z "$vcsprompt" ]] \
+		&& vcsprompt=$(hg_prompt_info)
+
+	echo "$vcsprompt"
 }
