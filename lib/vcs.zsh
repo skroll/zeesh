6bd0449b85c9ae81d5c89173b09ebf82e9981b26
Scott Kroll <skroll@gmail.com>
2011-06-30 16:04:40 -0400

    Rename variables.

--- lib/vcs.zsh
+++ lib/vcs.zsh
@@ -3,11 +3,11 @@ function vcs_prompt_info() {
 	## Try git first, then mercurial
 	vcsprompt=""
 
-	[[ "$DISABLE_ZEESH_GIT" != "true" ]] \
+	[[ "$ZEESH_DISABLE_GIT" != "true" ]] \
 		&& [[ -z "$vcsprompt" ]] \
 		&& vcsprompt=$(git_prompt_info)
 
-	[[ "$DISABLE_ZEESH_HG" != "true" ]] \
+	[[ "$ZEESH_DISABLE_HG" != "true" ]] \
 		&& [[ -z "$vcsprompt" ]] \
 		&& vcsprompt=$(hg_prompt_info)
 
