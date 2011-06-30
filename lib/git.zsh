662b0a6e5f59ec8e7eac398e025b609886a31124
Scott Kroll <skroll@gmail.com>
2011-06-30 11:35:17 -0400

    Cleanup of git lib.

--- lib/git.zsh
+++ lib/git.zsh
@@ -6,25 +6,23 @@ function git_prompt_info() {
 	echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
+## Checks the status of the git repository.
 parse_git_dirty() {
-	gitstat=$(git status 2>/dev/null | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
+	gitstat=$(git status --porcelain 2> /dev/null)
+	git_untracked_pattern="^\(??\| M\)"
+	git_dirty_pattern="^\(M \)"
 
-	if [[ $(echo ${gitstat} | grep -c "^# Changes to be committed:$") > 0 ]]; then
-		echo -n "$ZSH_THEME_GIT_PROMPT_DIRTY"
-	fi
+	[[ $(echo ${gitstat} | grep -c "${git_dirty_pattern}") > 0 ]] \
+		&& echo -n "$ZSH_THEME_GIT_PROMPT_DIRTY"
 
-	## Some versions of git use "Changed but not updated:" while others use
-	## "Changes not staged for commit:", so handle both here.
-	if [[ $(echo ${gitstat} | grep -c "^\(# Untracked files:\|# Changed but not updated:\|# Changes not staged for commit:\)$") > 0 ]]; then
-		echo -n "$ZSH_THEME_GIT_PROMPT_UNTRACKED"
-	fi 
+	[[ $(echo ${gitstat} | grep -c "${git_untracked_pattern}") > 0 ]] \
+		&& echo -n "$ZSH_THEME_GIT_PROMPT_UNTRACKED"
 
-	if [[ $(echo ${gitstat} | grep -v '^$' | wc -l | tr -d ' ') == 0 ]]; then
-		echo -n "$ZSH_THEME_GIT_PROMPT_CLEAN"
-	fi
+	[[ -z $(echo ${gitstat}) ]] \
+		&& echo -n "$ZSH_THEME_GIT_PROMPT_CLEAN"
 }
 
-## Checks if there are commits ahead from remote
+## Checks if there are commits ahead from remote.
 function git_prompt_ahead() {
 	if $(echo "$(git log origin/$(current_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
 		echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
@@ -46,35 +44,4 @@ function git_prompt_short_sha() {
 ## Formats prompt string for current git commit long SHA
 function git_prompt_long_sha() {
 	SHA=$(git rev-parse HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
-## Get the status of the working tree
-git_prompt_status() {
-	INDEX=$(git status --porcelain 2> /dev/null)
-	STATUS=""
-	if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
-	fi
-	if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-	elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-	fi
-	if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-	elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-	elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-	fi
-	if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
-	fi
-	if $(echo "$INDEX" | grep '^D ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-	fi
-	if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
-		STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
-	fi
-	echo $STATUS
 }
