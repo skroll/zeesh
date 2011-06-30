1d6575bafa8879d56e053bff8d47c981ed7eed8e
Scott Kroll <skroll@gmail.com>
2011-06-30 15:46:58 -0400

    Restrict vcs prompt to first hit.

--- lib/vcs.zsh
+++ lib/vcs.zsh
@@ -1,4 +1,6 @@
 ## Unified version control system prompt information
 function vcs_prompt_info() {
-	echo "$(git_prompt_info)$(hg_prompt_info)"
+	## Try git first, then mercurial
+	ref=$(git_prompt_info) || ref=$(hg_prompt_info) || ref=""
+	echo "${ref}"
 }
