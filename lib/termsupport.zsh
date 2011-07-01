e920de9766c27eee0ac08dd53ca23c5caa2f5632
Scott Kroll <skroll@gmail.com>
2011-07-01 09:22:10 -0400

    Minor clean up of termsupport lib.

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -5,14 +5,20 @@ function zeesh_window_title {
 	[ "$ZEESH_DISABLE_WINDOW_TITLE" != "true" ] || return
 
 	if [[ "$TERM" == screen* ]]; then
-		print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
-	elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
-		print -Pn "\e]2;$2:q\a" #set window name
-		print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
+		## Set screen hardstatus (around 20 characters).
+		print -Pn "\ek$1:q\e\\" 
+	elif [[ "$TERM" == xterm* ]] || \
+	     [[ "$TERM" == rxvt* ]] || \
+	     [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+		## Set window name.
+		print -Pn "\e]2;$2:q\a"
+		## Set tab name.
+		print -Pn "\e]1;$1:q\a"
 	fi
 }
 
-ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
+## 15 char left truncated path.
+ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<"
 ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
 
 function zeesh_window_title_precmd {
@@ -22,9 +28,12 @@ function zeesh_window_title_precmd {
 function zeesh_window_title_preexec {
 	emulate -L zsh
 	setopt extended_glob
-	local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
+
+	## Get the command name only (strip sudo or ssh).
+	local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
 	zeesh_window_title "$CMD" "%100>...>$2%<<"
 }
 
+## Add the functions to precmd and preexec
 precmd_functions+=zeesh_window_title_precmd
 preexec_functions+=zeesh_window_title_preexec
